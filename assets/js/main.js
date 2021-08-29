a3fa27d1b2fb05ac18b6430035d1826777e856e9
Reedip Adhikary <reedipadhikary@gmail.com>
2021-08-29 18:39:04 +0530

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,41 +112,6 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 /*==================== Contact message ====================*/
-// window.addEventListener("DOMContentLoaded", function () {
-//     var form = document.getElementById("my-form");
-//     var status = document.getElementById("status");
-
-//     function success() {
-//         form.reset();
-//         status.innerHTML = "Thanks! Message Sent."
-//     }
-
-//     function error() {
-//         status.innerHTML = "Oops! There was a problem."
-//     }
-
-//     form.addEventListener("submit", function (ev) {
-//         ev.preventDefault();
-//         var data = new FormData(form);
-//         ajax(form.method, form.action, data, success, error);
-//     });
-// });
-
-// function ajax(method, url, data, success, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader("Accept", "application/json");
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState !== XMLHttpRequest.DONE) return;
-//         if (xhr.status === 200) {
-//             success(xhr.response, xhr.responseType);
-//         } else {
-//             error(xhr.status, xhr.response, xhr.responseType)
-//         }
-//     };
-//     xhr.send(data);
-// }
-
 window.addEventListener("DOMContentLoaded", function () {
     // get the form elements defined in your form HTML above
 
@@ -196,10 +161,36 @@ function ajax(method, url, data, success, error) {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
 
